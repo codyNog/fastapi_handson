aa883e488dfd2cb89b48c1cab3893c1c06aee4d9
Tatsuya Hirase <tatch@jxpress.net>
2021-02-25 19:47:15 +0900

    デモ用に環境変数使ってみる

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import os
 from fastapi import FastAPI
 # import requests
 
 app = FastAPI()
 
+FOO = os.getenv("FOO", "set FOO as an environment variable to change me")
 
 @app.get("/")
 async def root():
+    print(FOO)
     # print(requests.get("http://sample-httpbin.sample/ip").text)
     return {"message": "hoge"}
